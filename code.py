11fa41a8c958f9f7da9fed8014c07fe42f53d7f5
Igor <igormuzetti@gmail.com>
2023-02-10 11:43:07 -0300

    Applying the Apache-2.0 license

--- code.py
+++ code.py
@@ -1,3 +1,21 @@
+# This code controls a prototype of an automatic irrigation system. 
+# The value used in the dry_value variable concerning our tests. 
+# We recommend calibrating this value for your environment.
+# 
+# Copyright 2023 Giuliane Eulália Corrêa
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import board
 import time
 
